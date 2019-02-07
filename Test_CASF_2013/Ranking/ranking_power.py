59dd69731cfbdc74707e6bd1d17fbbb9b0dfe736
Jianing Lu <jl7003@nyu.edu>
2019-02-07 14:08:46 -0500

    check CASF-2013 ranking

--- Test_CASF_2013/Ranking/ranking_power.py
+++ Test_CASF_2013/Ranking/ranking_power.py
@@ -19,15 +19,15 @@ def get_Ranking(score, relation = "positive"):
 
     """ Ranking Performance """
     refdata = pd.read_csv("Score_Rank.dat",sep = " ")
-    refdata.columns = ["#code, Prot, lig, pKd"]
+    refdata.columns = ["#code", "Prot", "lig", "pKd"]
     newdf = pd.merge(refdata,score,on = "#code")
     newdf["rank_org"] = newdf.groupby("Prot")["pKd"].rank(method = "min", ascending = False)
     newdf["rank_pre"] = newdf.groupby("Prot")["score"].rank(method = "min", ascending = False)
     ### High ###
     data = newdf[newdf["rank_org"] == newdf["rank_pre"]].groupby("Prot").size()
     success = len([ i for i in data if i == 3])
-    print("High Rank:" + str(n) + "," + str(round(n/65*100,1)))
-    High_Rank = str(round(n/65 *100,1))
+    print("High Rank:" + str(success) + "," + str(round(success/65*100,1)))
+    High_Rank = str(round(success/65 *100,1))
     High_Rank_list.append(High_Rank)
     ### Low ###
     data = newdf[newdf["rank_org"] == newdf["rank_pre"]][newdf["rank_org"].isin([1])]
@@ -40,10 +40,10 @@ if __name__ == "__main__":
     args = sys.argv[1:]
     datadir = args[0]
     score = args[1]
-    outfile = open(args[2], "w")
+    outfile = open(os.path.join(datadir,args[2]), "w")
     outfile.write("HR, LR\n")
     relation = args[3]
-    HR, LR = get_Ranking(os.path.join(datadir,score), relation = relation):
+    HR, LR = get_Ranking(os.path.join(datadir,score), relation = relation)
     outfile.write(str(HR) + "," + str(LR))
     outfile.close()
     #score = pd.read_csv(score, sep = " ")
--- Test_CASF_2013/Ranking/run_ranking_power.py
+++ Test_CASF_2013/Ranking/run_ranking_power.py
@@ -1,15 +1,15 @@
 import os
 
 def run_ranking(score_name, score_dir, relation):
-    os.system("python ranking_power.py " + score_dir +  " " + score_name + " " + score_name + ".out " + relation)
+    os.system("python ranking_power.py " + score_dir +  " " + score_name + " " + score_name.split(".")[0] + "_ranking.out " + relation)
 
 if __name__ == "__main__":
-    datadir = "/beegfs/jl7003/CASF-2016/power_scoring/our_score_0129"
+    datadir = "/scratch/jl7003/deltaVinaXGB_develop/Test_CASF_2013/Ranking/our_score_0129"
     #datadir = "/beegfs/jl7003/CASF-2016/power_scoring/other_score"
-    model_name = "model_16"
+    model_name = "model_17"
     #model_name = "RF"
     relation = "negative"
     for i in ["C","Co","Crwo","Cbwo"]:
-        score_dir = datadir + "/" + model_name + "/CASF-2013_Scoring_score_" + i + ".csv"
-        score_name = datadir + "/" + model_name + "/" + score_dir.split("/")[-1].split(".")[0]
-        run_scoring(score_name, score_dir, relation)
+        score_dir = datadir + "/" + model_name + "/"
+        score_name = "CASF-2013_Scoring_score_" + i + ".csv"
+        run_ranking(score_name, score_dir, relation)
